8bb6ab599869fcdc1b7424f61aaf01910b0324e4
martinbudden <mjbudden@gmail.com>
2026-08-11 10:50:45 +0100

    Added feature flags for PWM timers.

--- src/drivers_stm32.rs
+++ src/drivers_stm32.rs
@@ -1,4 +1,5 @@
 #![cfg(feature = "stm32")]
+#![allow(unused)]
 
 use embassy_stm32::timer::{
     GeneralInstance4Channel,
@@ -11,11 +12,51 @@ use crate::{
 };
 
 // TODO: sort out MotorDriverQuadPwmGeneral for stm32 variant
+#[cfg(feature = "motors_t8")]
+pub type MotorDriverQuadPwm = MotorDriverQuadPwmGeneral<embassy_stm32::peripherals::TIM8>;
+
+#[cfg(feature = "motors_t4_t3")]
 pub type MotorDriverQuadPwm =
-    MotorDriverQuadPwmGeneral<embassy_stm32::peripherals::TIM4, embassy_stm32::peripherals::TIM3>;
+    MotorDriverQuadPwmGeneral2<embassy_stm32::peripherals::TIM4, embassy_stm32::peripherals::TIM3>;
 
 #[allow(missing_debug_implementations, missing_copy_implementations)]
-pub struct MotorDriverQuadPwmGeneral<T1, T2>
+pub struct MotorDriverQuadPwmGeneral<T>
+where
+    T: GeneralInstance4Channel,
+{
+    ch0: SimplePwmChannel<'static, T>,
+    ch1: SimplePwmChannel<'static, T>,
+    ch2: SimplePwmChannel<'static, T>,
+    ch3: SimplePwmChannel<'static, T>,
+}
+
+impl<T> MotorDriverQuadPwmGeneral<T>
+where
+    T: GeneralInstance4Channel,
+{
+    pub fn new(pwm1: SimplePwm<'static, T>) -> Self {
+        let channels = pwm1.split();
+
+        Self { ch0: channels.ch1, ch1: channels.ch2, ch2: channels.ch3, ch3: channels.ch4 }
+    }
+
+    #[inline]
+    pub fn write_to_motors(&mut self, motor_outputs: MotorOutputs) {
+        let max_duty = 1000.0_f32;
+        self.ch0.set_duty_cycle(output_to_duty(motor_outputs[0], max_duty));
+        self.ch1.set_duty_cycle(output_to_duty(motor_outputs[1], max_duty));
+        self.ch2.set_duty_cycle(output_to_duty(motor_outputs[2], max_duty));
+        self.ch3.set_duty_cycle(output_to_duty(motor_outputs[3], max_duty));
+
+        self.ch0.enable();
+        self.ch1.enable();
+        self.ch2.enable();
+        self.ch3.enable();
+    }
+}
+
+#[allow(missing_debug_implementations, missing_copy_implementations)]
+pub struct MotorDriverQuadPwmGeneral2<T1, T2>
 where
     T1: GeneralInstance4Channel,
     T2: GeneralInstance4Channel,
@@ -26,16 +67,16 @@ where
     ch3: SimplePwmChannel<'static, T2>,
 }
 
-impl<T1, T2> MotorDriverQuadPwmGeneral<T1, T2>
+impl<T1, T2> MotorDriverQuadPwmGeneral2<T1, T2>
 where
     T1: GeneralInstance4Channel,
     T2: GeneralInstance4Channel,
 {
-    pub fn new(pwm1: SimplePwm<'static, T1>, pwm2: SimplePwm<'static, T2>) -> Self {
+    pub fn new2(pwm1: SimplePwm<'static, T1>, pwm2: SimplePwm<'static, T2>) -> Self {
         let channels1 = pwm1.split();
         let channels2 = pwm2.split();
 
-        Self { ch0: channels1.ch1, ch1: channels1.ch2, ch2: channels2.ch3, ch3: channels2.ch4 }
+        Self { ch0: channels1.ch1, ch1: channels1.ch2, ch2: channels2.ch1, ch3: channels2.ch2 }
     }
 
     #[inline]
